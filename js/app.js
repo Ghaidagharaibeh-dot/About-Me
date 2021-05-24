0cdcdc63c0eef2c990f8ff1fabb2377e3b23939d
Ghaidagharaibeh-dot <ghedaghraibeh@yahoo.com>
2021-05-24 17:20:26 +0300

    creating about me website

---
+++ js/app.js
@@ -0,0 +1,30 @@
+'use strict';
+alert('Welcome to my website');
+prompt('what is you name');
+prompt('What is your major');
+prompt('are you intersted in programming');
+alert('If you interested in programming sent me a massege in my Email to sent you a references to learn programming');
+
+
+let major= prompt('is my main major is industrial engineering ').toLowerCase();
+switch (major){
+   case 'yes':
+       case'y':
+       alert ('correct');
+       break;
+       case'no':
+       alert('incorrect'); 
+  default:
+       //console.log(you didnt answer my qoustion)
+       alert('you didnt answer my qoustion');          
+}
+
+
+
+
+
+
+     
+
+
+
